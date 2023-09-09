be87a199cab0c0347ddf02bb58120982b98e7d9c
nonplex <fuksvladislav@gmail.com>
2023-09-09 22:27:39 +0700

    Добавьте файлы проекта.

---
+++ enc_temp_folder/ce9faf9ed7c228d0a2adc210624afc83/laba 1 1.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+/**
+ * @brief Л/р 1 Знакомство со средой разработки
+ * Исходный код на C
+ * @return Всегда 0
+*/
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    printf("Hello World! Привет мир!\n");
+}
